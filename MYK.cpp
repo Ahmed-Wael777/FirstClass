e5dc47a66b756b1f386c34bcc019f63ace377352
YoussefDaoud <youssefdaoud0122@gmail.com>
2024-02-03 02:15:26 +0200

    class structure

---
+++ MYK.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+using namespace std;
+
+class FirstClass {
+private:
+    int l =5;
+    int w = 10;
+    int h =6;
+public:
+    int GetArea() {
+
+    }
+    int GetVolume() {
+
+    }
+
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
